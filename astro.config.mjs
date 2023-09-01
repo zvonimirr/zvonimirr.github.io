80560044bee56d1038ec28422854dea14179755d
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-09-01 07:36:42 +0200

    feat: add some integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,22 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
+import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://zvonimirr.github.io",
-  integrations: [tailwind(), astroImageTools],
+  integrations: [
+    tailwind(),
+    astroImageTools,
+    compress(),
+    sitemap(),
+    robotsTxt(),
+  ],
   experimental: {
     viewTransitions: true,
   },
 });
+
