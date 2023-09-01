7a70fbfb3b51151ba71541e203029c97d85689f3
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-09-01 07:33:55 +0200

    feat: use astro-imagetools for image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://zvonimirr.github.io",
-  integrations: [tailwind()],
+  integrations: [tailwind(), astroImageTools],
   experimental: {
     viewTransitions: true,
   },
