767e926c0b5bc63f533e81ddbfa2f37ed28fcc37
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-09-01 05:33:31 +0200

    feat: add projects page

---
+++ src/utils/projects.ts
@@ -0,0 +1,41 @@
+export const projects = [
+  {
+    title: "WhatsApp Link Generator",
+    description:
+      "A tool to generate WhatsApp links with custom messages and phone numbers.",
+    image: "/images/projects/wa-link-generator.png",
+    url: "wa-link-generator",
+    date: new Date("2017-12-01"),
+  },
+  {
+    title: "Raise Your Division",
+    description:
+      "A League of Legends boosting calculator which allows users to purchase the boost directly from the website.",
+    image: "/images/projects/raise-your-division.png",
+    url: "raise-your-division",
+    date: new Date("2017-09-01"),
+  },
+  {
+    title: "PHConvert",
+    description:
+      "A tool to convert Excel templates to a file format used by the Filipino government for tax purposes",
+    image: "/images/projects/phconvert.png",
+    url: "phconvert",
+    date: new Date("2020-10-01"),
+  },
+  {
+    title: "IndieScreening",
+    description: "A platform for indie filmmakers to showcase their work.",
+    image: "/images/projects/indiescreening.png",
+    url: "indiescreening",
+    date: new Date("2020-10-01"),
+  },
+  {
+    title: "Order Tracker",
+    description:
+      "An order tracker for a business in Turkey, with an admin dashboard for couriers.",
+    image: "/images/projects/order-tracker.png",
+    url: "order-tracker",
+    date: new Date("2021-08-01"),
+  },
+];
