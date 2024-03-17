e7a37b7325fa433788bd7a799992e7f85f9ae543
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-03-17 23:57:30 +0100

    feat: animate dropdowns

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,6 +12,20 @@ export default {
       fontFamily: {
         squada: ["SquadaOne", ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {
+        extend: {
+          "0%": { transform: "scaleY(0)" },
+          "100%": { transform: "scaleY(1)" },
+        },
+        collapse: {
+          "0%": { transform: "scaleY(1)" },
+          "100%": { transform: "scaleY(0)" },
+        },
+      },
+      animation: {
+        extend: "extend 0.5s ease-in-out",
+        collapse: "collapse 0.5s ease-in-out",
+      },
     },
   },
   plugins: [],
