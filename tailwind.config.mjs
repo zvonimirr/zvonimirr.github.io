097eb71192cf3b127b08221b5fcb94863ac5a951
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-03-17 22:51:23 +0100

    feat: add custom font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#171A21",
+        secondary: "#B0FE76",
+      },
+      fontFamily: {
+        squada: ["SquadaOne", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
