8fa24871f44c690174944511760e51f1682f702e
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2024-03-18 00:26:02 +0100

    feat: use css transitions

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,19 +12,8 @@ export default {
       fontFamily: {
         squada: ["SquadaOne", ...defaultTheme.fontFamily.sans],
       },
-      keyframes: {
-        extend: {
-          "0%": { transform: "scaleY(0)" },
-          "100%": { transform: "scaleY(1)" },
-        },
-        collapse: {
-          "0%": { transform: "scaleY(1)" },
-          "100%": { transform: "scaleY(0)" },
-        },
-      },
-      animation: {
-        extend: "extend 0.5s ease-in-out",
-        collapse: "collapse 0.5s ease-in-out",
+      transitionProperty: {
+        "max-height": "max-height",
       },
     },
   },
